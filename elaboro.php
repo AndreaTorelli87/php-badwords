b6e3d94b11bcfc345f7a7766e9e666733b0c58c7
Andrea Torelli <gioran326@protonmail.com>
2023-04-17 22:10:57 +0200

    Aggiunta un minimo di grafica

--- elaboro.php
+++ elaboro.php
@@ -1,19 +1,33 @@
 <?php
-   $frase = $_GET['frase'];
-   $parola = $_GET['parola'];
+$frase = $_GET['frase'];
+$parola = $_GET['parola'];
 
-   $risultato = str_replace($parola, "***", $frase)
-?>
+$risultato = str_replace($parola, "***", $frase)
+   ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Bad Words</title>
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+   <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-   <h1><?php echo $frase ." è lunga: " . strlen($frase); ?></h1>
-   <h2><?php echo $risultato; ?></h2>
+   <div class="container p-5 text-center">
+      <h1 class="mb-4 fw-bold bg-warning p-4 d-inline-block rounded-5 text-danger">
+         <?php echo $frase; ?>
+      </h1>
+      <br />
+      <h2 class="mb-4 fw-bold bg-warning p-4 d-inline-block rounded-5 text-danger">
+         <?php echo $risultato . " <-- è lunga: " . strlen($risultato) . " caratteri"; ?>
+      </h2>
+      <br />
+      <a href="index.php" class="btn btn-primary mt-4">Back</a>
+   </div>
 </body>
+
 </html>
--- index.php
+++ index.php
@@ -5,12 +5,18 @@
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Bad Words</title>
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+   <link rel="stylesheet" href="style.css">
 </head>
 <body>
-   <form action="elaboro.php" method="get" action="elaboro.php">
-      <input type="text" name="frase" placeholder="Inserisci una frase">
-      <input type="text" name="parola" placeholder="Parola vietata">
-      <button type="submit">Invia</button>
-   </form>
+   <div class="container p-5 text-center">
+      <h1 class="mb-4 display-1 fw-bold bg-warning p-4 d-inline-block rounded-5 text-danger">Bad Words</h1>
+      <form action="elaboro.php" method="get" action="elaboro.php">
+         <input type="text" name="frase" placeholder="Inserisci una frase" required>
+         <input type="text" name="parola" placeholder="Parola vietata" required>
+         <br />
+         <button type="submit" class="btn btn-primary mt-4">Invia</button>
+      </form>
+   </div>
 </body>
 </html>
